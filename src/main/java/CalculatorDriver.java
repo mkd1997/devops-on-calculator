e09130db454140deff3f1f1af8ebe4040868f736
mkd1997 <manavdesai1997@gmail.com>
2020-04-05 19:59:26 +0530

    code build success

---
+++ src/main/java/CalculatorDriver.java
@@ -0,0 +1,8 @@
+import com.mkd.devopsdemo.*;
+
+public class CalculatorDriver {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(5,6));
+    }
+}
